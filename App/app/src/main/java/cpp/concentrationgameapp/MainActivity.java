9b9ea90d3eb43daf71dc3187c6730df181d7de8d
Leonardo Davalos <leonardodavalos3@gmail.com>
2022-11-30 17:00:17 -0800

    MainActivity Finishes after starting homeActivity

--- App/app/src/main/java/cpp/concentrationgameapp/MainActivity.java
+++ App/app/src/main/java/cpp/concentrationgameapp/MainActivity.java
@@ -18,9 +18,9 @@ public class MainActivity extends AppCompatActivity {
             () -> {
                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
+                this.finish();
             }, 3000
         );
-
 
 
     }
