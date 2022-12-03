354e2d6caa0be47a4b4ab9b6c65333375478f085
Matthew Finerty <mfinerty@cpp.edu>
2022-12-03 15:36:18 -0800

    Create HighScore class with JSON serialization methods

---
+++ App/app/src/main/java/cpp/concentrationgameapp/HighScore.java
@@ -0,0 +1,47 @@
+package cpp.concentrationgameapp;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class HighScore {
+
+    private String name;
+    private int score;
+    private int tileCount;
+
+    public HighScore(String name, int score, int tileCount) {
+        this.name = name;
+        this.score = score;
+        this.tileCount = tileCount;
+    }
+
+    public static HighScore fromJSON(JSONObject json) {
+        try {
+            return new HighScore(
+                    json.getString("name"),
+                    json.getInt("score"),
+                    json.getInt("tileCount")
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to create high score from JSON object.");
+        }
+    }
+    
+    public JSONObject toJSON() {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("name", name);
+            json.put("score", score);
+            json.put("tileCount", tileCount);
+            return json;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to create JSON object from high score.");
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
