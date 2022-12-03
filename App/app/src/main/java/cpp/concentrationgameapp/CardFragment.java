e3c4f5370b3136207c5b437af5e6e7b334acbdb2
Matthew Finerty <mfinerty@cpp.edu>
2022-12-03 04:35:44 -0800

    Create basic card layout for game

---
+++ App/app/src/main/java/cpp/concentrationgameapp/CardFragment.java
@@ -0,0 +1,63 @@
+package cpp.concentrationgameapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CardFragment extends Fragment implements View.OnClickListener {
+
+    private String word;
+    private TextView textView;
+    private ImageView cardBack;
+    private boolean flipped;
+
+    public static CardFragment newCard(String word) {
+        CardFragment cardFragment = new CardFragment();
+        Bundle args = new Bundle();
+        args.putString("word", word);
+        cardFragment.setArguments(args);
+        return cardFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        word = args.getString("word");
+
+        return inflater.inflate(R.layout.card_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = view.findViewById(R.id.word);
+        textView.setText(word);
+        cardBack = view.findViewById(R.id.cardBack);
+        getView().setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        System.out.println(word);
+        flip();
+    }
+
+    public void flip() {
+        if (flipped) {
+            cardBack.setVisibility(View.VISIBLE);
+            textView.setVisibility(View.INVISIBLE);
+        } else {
+            cardBack.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.VISIBLE);
+        }
+        flipped = !flipped;
+
+
+    }
+}
--- App/app/src/main/java/cpp/concentrationgameapp/GameActivity.java
+++ App/app/src/main/java/cpp/concentrationgameapp/GameActivity.java
@@ -1,12 +1,15 @@
 package cpp.concentrationgameapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class GameActivity extends AppCompatActivity {
 
     private AudioHandler audioHandler;
+    private TableLayout cardGrid;
     private int tileCount;
 
     @Override
@@ -20,6 +23,27 @@ public class GameActivity extends AppCompatActivity {
         }
 
         tileCount = getIntent().getIntExtra("tiles", 4);
+
+        cardGrid = findViewById(R.id.cardTable);
+        for (int i = 0; i < 4; i++) {
+            TableRow row = new TableRow(this);
+            row.setGravity(Gravity.CENTER);
+            row.setId(7000 + i);
+            row.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.WRAP_CONTENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT,
+                    1
+            ));
+
+            for (int x = 0; x < 4; x++) {
+                CardFragment cardFragment = CardFragment.newCard("Card " + i + " " + x);
+                getFragmentManager().beginTransaction()
+                        .add(row.getId(), cardFragment, null)
+                        .commit();
+            }
+
+            cardGrid.addView(row);
+        }
 
         if(getSupportActionBar() != null){
             getSupportActionBar().hide();
